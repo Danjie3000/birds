ec5adc782a4f96ba67e34f13a85816f93acc89f6
Danjie3000 <90595974+Danjie3000@users.noreply.github.com>
2023-02-21 22:41:19 +0100

    resten af crud

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 console.log(app);
 
-const birds3 = [
+const birdFam = [
     { 
         id: '1', 
         name: 'Crow'
@@ -24,27 +27,44 @@ app.get('/bird1', (req, res) => {
 });
 
 app.get('/birds', (req, res) => {
-    res.send(birds3);
+    res.send(birdFam);
 });
 
 app.get('/birds/:id', (req, res) => {
     const id = req.params.id;
-    const bird = birds3.find(bird => bird.id === id);
+    const bird = birdFam.find(bird => bird.id === id);
+    if (!bird) return res.status(404).send({message: 'Bird not found'});
     res.send(bird);
 });
-/*
-app.post('/', (req, res) => {
- 
+
+app.post('/makebird', (req, res) => {
+    const bird = {
+        id: birdFam.length + 1,
+        name: req.body.name
+    };
+    birdFam.push(bird);
+    res.send(bird);
 });
 
-app.patch('/', (req, res) => {
- 
+app.put('/updatebird', (req, res) => {
+    // Find fugl. Hvis fugl ikke findes, send 404.
+    const bird = birdFam.find(bird => bird.id === id);
+    if (!bird) return res.status(404).send({message: '404. Bird not found'});
+
+    bird.name = req.body.name;
+    res.send(bird);
 });
 
-app.delete('/', (req, res) => {
- 
+app.delete('/deletebird/:id', (req, res) => {
+    const bird = birdFam.find(bird => bird.id === id);
+    if (!bird) return res.status(404).send({message: '404. Bird not found'});
+
+    const index = birdFam.indexOf(bird);
+    birdFam.splice(index, 1);
+
+    res.send(bird);
 });
-*/
+
 console.log("Server is running on port 8080");
 
 app.listen(8080);
